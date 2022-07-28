75c53b905ad11ac93c55a22c103ce480dee89d87
Rohaan Telangur <94789421+rohaantelangur@users.noreply.github.com>
2022-07-28 19:09:58 +0530

    Form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Form from './Components/Form';
 import Home from './Components/Home';
+import Test from './Components/Test';
 
 function App() {
   return (
-    <Home/>
+    // <Home/>
+    // <Form/>
+    <Test />
   );
 }
 
---
+++ src/Components/Form.jsx
@@ -0,0 +1,23 @@
+import { Box, HStack } from '@chakra-ui/react'
+import React from 'react'
+
+const Form = () => {
+  return (
+    <Box>
+        <HStack w="90%" h="500px" m="auto" border="1px solid red" spacing={0}>
+            <Box h="500px" w="50%" border="1px solid red">
+                
+
+
+            </Box>
+            <Box h="500px" w="50%" border="1px solid red">
+
+            </Box>
+
+        </HStack>
+
+    </Box>
+  )
+}
+
+export default Form
